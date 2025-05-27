b26a080d81cbebc0caf8755d6cd45b673173d06b
ArvyKrom <arvydaskromelis@gmail.com>
2025-05-27 23:15:56 +0300

    Updated PID files

--- STM32/Peltier_Controller_G0B0/Core/Inc/pid.h
+++ STM32/Peltier_Controller_G0B0/Core/Inc/pid.h
@@ -1,14 +1,20 @@
 #ifndef __PID_H
 #define __PID_H
 
-#define Kp 10
-#define Ki 0.05
-#define Kd 500
+#define Kp 4.5
+#define Ti 100
+#define Kd 550
+#define Td 47
+#define Ki 0.01
 #define dT 1 // seconds
-#define integral_cap 50
+#define integral_cap 10.5
+#define Number_Of_points_For_Kd_calc 50
+#define Ki_introduction_point 0.90
+// limit that defines how close to the setpoint the integral should be introduced
+#define Integral_max_deviation 2
 
-double compute_pid_output(double error);
-double compute_derivative_part(double current_error, double *error_history, int size);
-int push_new_value_from_the_end(double new_val, double *array, int size);
+double compute_pid_output(double current_temp, double set_temp, double outside_temp, double *Kp_part, double *Ki_part, double *Kd_part);
+double compute_derivative_part(double current_error, double *error_history, int points_collected_since_set_temp_reset);
+double assign_integral_value(double set_temp, double outside_temp);
 
 #endif
--- STM32/Peltier_Controller_G0B0/Core/Src/pid.c
+++ STM32/Peltier_Controller_G0B0/Core/Src/pid.c
@@ -1,30 +1,82 @@
 #include <stdlib.h>
+#include <string.h>
 #include "pid.h"
 
-double compute_pid_output(double error){
+// Temp points (realtive to room temperature) where we'll estimate PID integral part values
+static int Integral_Temp_Table[] = {-15,-10,-5,0,5,10,15,20,25,30,35,40};
+// These values have not been found experimentally but rather are assigned logically and roughly making asssumptionns
+static double Integral_Part_Table[] = {-12,-8,-4,0,1.5,3,4.5,6,7.5,9,10.5,12};
+
+double compute_pid_output(double current_temp, double set_temp, double outside_temp, double *Kp_part, double *Ki_part, double *Kd_part){
+	static int integral_on = 0;
 	static double integral = 0;
-	static double previous_error[10] = {0};
-	double pid_output = Kp*error + Ki*integral + Kd*compute_derivative_part(error, previous_error, 10);
-	push_new_value_from_the_end(error, previous_error, 10);
-	if(abs(integral + error * dT) < integral_cap){
+	static double previous_error[Number_Of_points_For_Kd_calc] = {0};
+	static double previous_set_temp = 0;
+	static int points_collected_since_set_temp_reset = 0;
+	static double integral_estimate = 0;
+	double error = 0;
+
+	if(set_temp != previous_set_temp){
+		memset(previous_error, 0, Number_Of_points_For_Kd_calc*sizeof(double));
+		points_collected_since_set_temp_reset = 0;
+		integral_on = 0;
+		integral = 0;
+	}
+
+	previous_set_temp = set_temp;
+	error = set_temp - current_temp;
+
+	if( (abs(current_temp - set_temp) / set_temp) <= 1 - Ki_introduction_point && integral_on == 0){
+		integral_estimate = assign_integral_value(set_temp, outside_temp);
+		integral = integral_estimate;
+		integral_on = 1;
+	}
+
+	if(integral_on && abs(Ki*integral_estimate - (Ki*(integral + error * dT))) <= Integral_max_deviation ){
 		integral += error * dT;
 	}
-	return pid_output;
+
+	(*Kp_part) = Kp * error;
+	(*Ki_part) = Ki * integral;
+	(*Kd_part) = Kd * compute_derivative_part(error, previous_error, points_collected_since_set_temp_reset);
+	points_collected_since_set_temp_reset++;
+
+	return (*Kp_part) + (*Ki_part) + (*Kd_part);
 }
 
-double compute_derivative_part(double current_error, double *error_history, int size){
-	for(int i = 0;i<size;i++){
-		if(current_error - error_history[size-1-i] != 0){
-			return (current_error - error_history[size-1-i])/(dT*(1+i));
+double assign_integral_value(double set_temp, double outside_temp){
+	double temp_diff = set_temp - outside_temp;
+	double previous_closeness = 999;
+	for(int i = 0; i < 12; i++){
+		if (abs(temp_diff - Integral_Temp_Table[i]) < previous_closeness){
+			previous_closeness = abs(temp_diff - Integral_Temp_Table[i]);
+		}
+		else{
+			return Integral_Part_Table[i-1] / Ki;
 		}
 	}
-	return 0.0;
-}
-
-int push_new_value_from_the_end(double new_val, double *array, int size){
-	for(int i = size-2;i>=0; i--){
-		array[i] = array[i+1];
-	}
-	array[size - 1] = new_val;
 	return 0;
 }
+
+double compute_derivative_part(double current_error, double *error_history, int points_collected_since_set_temp_reset){
+	double avg_speed = 0;
+
+	// Push every element to the left and add to the right
+	for(int i = 0; i < Number_Of_points_For_Kd_calc -1; i++){
+		error_history[i] = error_history[i+1];
+	}
+	error_history[Number_Of_points_For_Kd_calc-1] = current_error;
+
+	// Calc the avg speed
+	double earliest_error = 0;
+	if (points_collected_since_set_temp_reset < Number_Of_points_For_Kd_calc){
+		earliest_error = error_history[Number_Of_points_For_Kd_calc - 1 - points_collected_since_set_temp_reset];
+	}
+	else{
+		earliest_error = error_history[0];
+	}
+	avg_speed = (error_history[Number_Of_points_For_Kd_calc-1] - earliest_error) / (Number_Of_points_For_Kd_calc*dT);
+	return avg_speed;
+}
+
+
